ec5598d1741ee5fdf46cd0a4a0e25314ecba5638
Mitzi Vite <vit20001@byui.edu>
2023-09-15 20:01:27 -0600

    Grades change

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Collections.Specialized;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade? ");
+        string grade = Console.ReadLine();
+        int number = int.Parse(grade);
+        string letter = "";
+        
+
+        if (number >= 90)
+        {
+            letter = "A";
+        }
+        else if (number >= 80)
+        {
+            letter = "B";
+        } 
+        else if (number >= 70)
+        {
+            letter = "C";
+        }
+        else if (number >= 60) 
+        { 
+            letter = "D";
+        }
+        else if (number < 60)
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"You're grade is {letter}");
+
+        if (number > 70)
+        {
+            Console.WriteLine("Congrats you passed the class");
+        }
+        else if (number < 70 )
+        {
+            Console.WriteLine("You didn't pass. But, it is ok, you will get it next time.");
+        }
     }
 }
