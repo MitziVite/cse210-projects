318078a92153d5bb7f16a26bb8c70a62d3d9dbe0
Mitzi Vite <vit20001@byui.edu>
2023-09-19 22:32:02 -0600

    prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,36 @@
 using System;
-
+using System.Collections.Generic;
+using System.Linq;
 class Program
+
 {
     static void Main(string[] args)
+    
     {
-        Console.WriteLine("Hello Prep4 World!");
+        
+        List<int> numbers = new List<int>();
+        int userNumber;
+        
+        do{
+        Console.WriteLine("Enter number");
+        userNumber = int.Parse(Console.ReadLine());
+        if (userNumber != 0)
+    {
+        numbers.Add(userNumber);
+    }
+        }while (userNumber != 0);
+
+        int total = 0;
+        
+        foreach (int number in numbers)
+        {
+            total += number;
+            
+        }
+        double avg = (double)total / numbers.Count;
+        int maxNumber = numbers.Max();
+        Console.WriteLine($"The sum is {total}");
+        Console.WriteLine($"The Average is {avg}");
+        Console.WriteLine($"The largest number is: {maxNumber}");
     }
 }
