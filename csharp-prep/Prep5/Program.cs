4a29a73144aa82b68ad3bd721f1c68601509b14d
Mitzi Vite <vit20001@byui.edu>
2023-09-20 14:07:08 -0600

    Prep 5 finish

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        double value1 = SquareNumber(userNumber);
+        Console.Write($"{userName}, the square of your number is {value1}");
     }
+    static void DisplayWelcome() 
+    {
+        Console.Write("Welcome to the program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int userNumber = int.Parse(Console.ReadLine());
+        return userNumber;
+    }
+
+    static double SquareNumber(double userNumber)
+    {
+        double value1 = Math.Pow(userNumber, 2);
+        return value1;
+    }
+
 }
